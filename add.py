33bf748b9eb0bcfd8f93acf81be176ff2e23102b
lei xin <chjgreen@qq.com>
2019-07-06 21:18:16 +0800

    命令行模式

---
+++ add.py
@@ -0,0 +1,11 @@
+import hug
+
+
+@hug.cli()
+def add(number_1: hug.types.number, number_2: hug.types.number):
+    """Returns the result of adding number_1 to number_2"""
+    return number_1 + number_2
+
+
+if __name__ == '__main__':
+    add.interface.cli()
