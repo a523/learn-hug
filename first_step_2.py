5b0faf6b0f4d4f0f0b58de9b255b65b72afde89e
lei xin <chjgreen@qq.com>
2019-08-11 14:15:28 +0800

    增加自定义异常

--- first_step_2.py
+++ first_step_2.py
@@ -1,8 +1,35 @@
 import hug
+from falcon import HTTPError, HTTP_401, HTTPStatus
 
 
+class MyErr(HTTPError):
+    def __init__(self, desc):
+        super().__init__(status=HTTP_401, title='err', description=str(desc))
+
+
+@hug.exception(exclude=MyErr)
+def handle_exception(exception, response):
+    response.status = HTTP_401
+    return {'err': '{}'.format(exception)}
+
+
+@hug.cli()
 @hug.get(examples='name=Timothy&age=26')
 def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
+    if age < 0:
+        raise MyErr
+    if age == 0:
+        raise Exception('is zero')
 
-    return  {'message': f'Happyr {name} Birthday {age}!',
-             'took': float(hug_timer)}
+    try:
+        with open(name, 'r') as f:
+            f.readlines()
+
+    except FileNotFoundError as e:
+        raise MyErr(e)
+    return {'message': f'Happy {name} Birthday {age}!',
+            'took': float(hug_timer)}
+
+
+if __name__ == '__main__':
+    happy_birthday.interface.cli()
